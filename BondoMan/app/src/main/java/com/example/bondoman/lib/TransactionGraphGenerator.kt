e38c670bb0e5be5b1bed4e3500227a1f5b3b6349
Haziq <haziqabiyyu@gmail.com>
2024-03-11 22:26:23 +0700

    chore: move module to root

--- BondoMan/app/src/main/java/com/example/bondoman/lib/TransactionGraphGenerator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bondoman.lib
-
-import com.example.bondoman.entities.Transaction
-
-class TransactionGraphGenerator: ITransactionGraphGenerator {
-    override fun generateGraph(transactions: Array<Transaction>) {
-        TODO("Not yet implemented")
-    }
-}
