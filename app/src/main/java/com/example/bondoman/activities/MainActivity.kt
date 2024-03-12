cdc2622822670384f8c11005917bf9f4e3cdfc0a
Natthan Krish <natthankrish@Nates-MacBook-Pro.local>
2024-03-12 22:10:30 +0700

    refactor: fragment

--- app/src/main/java/com/example/bondoman/activities/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/activities/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.bondoman.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.bondoman.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView
+                >(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/bondoman/fragments/TwibbonFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/TwibbonFragment.kt
@@ -1,21 +1,20 @@
-package com.example.bondoman
+package com.example.bondoman.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.example.bondoman.R
 
 /**
  * A simple [Fragment] subclass.
  * Use the [TwibbonFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 class TwibbonFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
