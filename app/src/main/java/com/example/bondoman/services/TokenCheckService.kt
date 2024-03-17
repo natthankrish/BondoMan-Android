8e234529b107dd4a7343657a9a0f5a1eaea46b2b
nathaniacalista01 <nathania.calista01@gmail.com>
2024-03-17 16:57:15 +0700

    feat : logout if jwt expired

--- app/src/main/java/com/example/bondoman/services/TokenCheckService.kt
+++ app/src/main/java/com/example/bondoman/services/TokenCheckService.kt
@@ -5,41 +5,61 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import com.example.bondoman.apiServices.IAuthService
+import com.example.bondoman.lib.SecurePreferences
 import com.example.bondoman.retrofits.LoginRetro
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class TokenCheckService(private val token : String): Service() {
+class TokenCheckService: Service() {
     private val serviceScope = CoroutineScope(Dispatchers.IO)
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
         serviceScope.launch {
             while (true){
-                checkToken(token)
-                delay(5000)
+                val securePreferences = SecurePreferences(applicationContext)
+                val token = securePreferences.getToken()
+                val isValid = checkToken(token)
+                if(!isValid){
+                    sendTokenExpiredBroadcast()
+                    securePreferences.clearToken()
+                    stopSelf()
+                    break
+                }else{
+                    delay(5000)
+                }
             }
         }
         return START_STICKY
     }
 
-    private suspend fun checkToken(userToken : String){
+    private suspend fun checkToken(userToken : String?) : Boolean{
         val tokenService = LoginRetro().getRetroClientInstance().create(IAuthService::class.java)
-        try {
+        return try {
             val response = tokenService.checkToken("Bearer $userToken")
-            if(response.nim != null){
-                Log.d("Token", "Token is not expired")
-            }else{
+            if(response.nim == null){
                 Log.d("Expired", "Token is expired")
+                false
+            }else{
+                Log.d("Token", "Token is not expired")
+                true
             }
 
         }catch (e : Exception){
             e.printStackTrace()
+            return false
         }
     }
 
+    private fun sendTokenExpiredBroadcast(){
+        Intent().also { intent ->
+            intent.action = "com.example.bondoman.TOKEN_EXPIRED"
+            sendBroadcast(intent)
+        }
+    }
 
 }
