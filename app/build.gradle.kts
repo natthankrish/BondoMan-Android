056c709b1f32705f7f36c02eccf7d76c03f26310
Natthan Krish <natthankrish@Neos-MacBook-Pro.local>
2024-03-30 21:22:40 +0700

    fix: no action bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,15 @@ dependencies {
     val material_version = "1.11.0"
     implementation("com.google.android.material:material:$material_version")
 
+    val camerax_version = "1.3.2"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
