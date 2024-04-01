eb956aef0958ebfba6eb6bddb0f4832c4dfbee70
Nathania Calista <nathania.calista01@gmail.com>
2024-04-01 21:14:23 +0700

    feat : logout function

--- app/src/main/java/com/example/bondoman/activities/LoginActivity.kt
+++ app/src/main/java/com/example/bondoman/activities/LoginActivity.kt
@@ -1,16 +1,21 @@
 package com.example.bondoman.activities
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.airbnb.lottie.LottieAnimationView
 import com.example.bondoman.R
 import com.example.bondoman.apiServices.IAuthService
 import com.example.bondoman.lib.SecurePreferences
-import com.example.bondoman.repositories.LoginRepository
+import com.example.bondoman.repositories.AuthRepository
 import com.example.bondoman.retrofits.Retro
 import kotlinx.coroutines.launch
 
@@ -18,30 +23,50 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var email : EditText
     private lateinit var password : EditText
     private lateinit var loginButton: Button
-    private lateinit var loginRepository: LoginRepository
+    private lateinit var loginRepository: AuthRepository
+    private var loadingDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val authService = Retro().getRetroClientInstance().create(IAuthService::class.java)
         val securePreferences = SecurePreferences(this)
 
-        loginRepository = LoginRepository(authService, securePreferences)
+        loginRepository = AuthRepository(securePreferences)
 
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
         loginButton = findViewById<Button>(R.id.login_button)
         loginButton.setOnClickListener{
             lifecycleScope.launch {
-                val result = loginRepository.login(email.text.toString(), password.text.toString())
-                if(result.isSuccess){
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(intent)
-                }else{
-                    val message = result.exceptionOrNull()?.message ?: "Login failed!"
-                    Toast.makeText(this@LoginActivity, message, Toast.LENGTH_LONG).show()
-                }
+                login(email.text.toString(), password.text.toString())
             }
         }
+    }
+
+    private suspend fun login(email : String, password : String){
+        showLoadingDialog()
+        val result = loginRepository.login(email, password)
+        hideLoadingDialog()
+        if(result.isSuccess){
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            val message = result.exceptionOrNull()?.message ?: "Login failed!"
+            Toast.makeText(this@LoginActivity, message, Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun showLoadingDialog() {
+        loadingDialog = Dialog(this).apply {
+            setContentView(R.layout.loading)
+            setCancelable(false)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            show()
+        }
+    }
+
+    private fun hideLoadingDialog() {
+        loadingDialog?.dismiss()
     }
 }
--- app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
@@ -1,21 +1,29 @@
 package com.example.bondoman.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.example.bondoman.R
+import com.example.bondoman.activities.LoginActivity
+import com.example.bondoman.activities.MainActivity
 import com.example.bondoman.database.TransactionDatabase
 import com.example.bondoman.databinding.FragmentSettingsBinding
 import com.example.bondoman.entities.Transaction
 import com.example.bondoman.lib.ITransactionFileAdapter
+import com.example.bondoman.lib.SecurePreferences
 import com.example.bondoman.lib.TransactionDownloader
 import com.example.bondoman.lib.TransactionExcelAdapter
+import com.example.bondoman.repositories.AuthRepository
 import com.example.bondoman.repositories.TransactionRepository
 import com.example.bondoman.viewModels.TransactionViewModelFactory
 import com.example.bondoman.viewModels.TransactionsViewModel
@@ -25,6 +33,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -41,10 +50,11 @@ class SettingsFragment : Fragment() {
             ).transactionDao())
         )
     }
+    private lateinit var authRepository: AuthRepository
     private lateinit var transactions: List<Transaction>
     private lateinit var transactionFileAdapter: ITransactionFileAdapter
     private lateinit var transactionDownloader: TransactionDownloader
-
+//    private var authRepository : AuthRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         transactionViewModel.allTransaction.observe(this) {
@@ -52,6 +62,7 @@ class SettingsFragment : Fragment() {
         }
         transactionFileAdapter = TransactionExcelAdapter()
         transactionDownloader = TransactionDownloader()
+        authRepository =  AuthRepository(SecurePreferences(requireContext()))
     }
 
     override fun onCreateView(
@@ -88,8 +99,36 @@ class SettingsFragment : Fragment() {
                 binding.saveButton.isClickable = true
             }
         }
+        binding.logoutButton.setOnClickListener{
+            lifecycleScope.launch {
+                showLoading()
+                logout()
+                hideLoading()
+            }
+        }
     }
 
+    private suspend fun logout() {
+        val response = authRepository.logout()
+        if (response.isSuccess) {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            requireActivity().finish()
+        }else{
+            withContext(Dispatchers.Main) {
+                val layoutInflater = LayoutInflater.from(requireContext())
+                val view = layoutInflater.inflate(R.layout.custom_toast, null)
+                val textView = view.findViewById<TextView>(R.id.customToastText)
+                textView.text = "Logout failed, please try again!"
+                with (Toast(requireContext())) {
+                    duration = Toast.LENGTH_LONG
+                    setView(view)
+                    show()
+                }
+            }
+        }
+    }
     private fun showLoading() {
         binding.loadingAnimation.isVisible = true
     }
