e92bfe53f037b0db012293c716e768b64a76df22
Haziq <haziqabiyyu@gmail.com>
2024-04-01 10:47:38 +0700

    fix: gmail attachment issue

--- app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
@@ -54,7 +54,7 @@ class SettingsFragment : Fragment() {
     private lateinit var transactions: List<Transaction>
     private lateinit var transactionFileAdapter: ITransactionFileAdapter
     private lateinit var transactionDownloader: TransactionDownloader
-    private val XSLX_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    private val XLSX_MIME_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,14 +79,14 @@ class SettingsFragment : Fragment() {
         binding.saveButton.setOnClickListener {
             binding.saveButton.isClickable = false
             val context = requireContext()
-            val fileName = createFileName(transactions)
+            val fileName = createFileName(transactions, "xlsx")
             this.lifecycleScope.launch {
                 val result = async(Dispatchers.IO) {
                     return@async transactionDownloader.downloadTransactionAsFile(
                         context,
                         fileName,
                         transactions,
-                        XSLX_MIME_TYPE,
+                        XLSX_MIME_TYPE,
                         transactionFileAdapter
                     )
                 }
@@ -101,19 +101,21 @@ class SettingsFragment : Fragment() {
         }
         binding.sendButton.setOnClickListener {
             val context = requireContext()
-            val fileName = createFileName(transactions)
+            val fileName = createFileName(transactions, "xlsx")
             val file = File(requireContext().externalCacheDir, fileName)
             val outputStream = FileOutputStream(file)
+
             outputStream.use {
                 transactionFileAdapter.save(transactions, fileName, it)
             }
             composeEmail(
                 arrayOf(transactions.getOrNull(0)?.userEmail ?: "13521170@std.stei.itb.ac.id"),
-                "Test Subject",
-                file.toUri()
+                "Bondoman Transaction Summary",
+                "Here's your latest transaction summary",
+                FileProvider.getUriForFile(requireContext(), requireContext().applicationContext.packageName + ".provider", file)
             )
-            }
         }
+    }
 
     private fun showLoading() {
         binding.loadingAnimation.isVisible = true
@@ -123,15 +125,15 @@ class SettingsFragment : Fragment() {
         binding.loadingAnimation.isVisible = false
     }
 
-    private fun createFileName(transactions: List<Transaction>): String {
-        val dateFormat = SimpleDateFormat("dd MM yyyy HH:mm:ss:SSS", Locale.getDefault())
+    private fun createFileName(transactions: List<Transaction>, extension: String): String {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy HH:mm:ss:SSS", Locale.getDefault())
         val currentTime = dateFormat.format(Date())
 
         val userEmail = transactions.getOrNull(0)?.userEmail ?: "UnknownUser"
         val userName = userEmail.split("@").firstOrNull() ?: "UnknownUser"
         val fileName = "$currentTime $userName Transaction Summary"
 
-        return "$fileName.xlsx"
+        return "$fileName.$extension"
     }
 
     private fun showSnackbar(message: String) {
@@ -142,13 +144,15 @@ class SettingsFragment : Fragment() {
 
     }
 
-    fun composeEmail(addresses: Array<String>, subject: String, attachment: Uri) {
-        val intent = Intent(Intent.ACTION_SENDTO).apply {
-            data = Uri.parse("mailto:")
-            putExtra(Intent.EXTRA_EMAIL, addresses)
-            putExtra(Intent.EXTRA_SUBJECT, subject)
-            putExtra(Intent.EXTRA_STREAM, attachment)
-        }
+    fun composeEmail(addresses: Array<String>, subject: String, text: String, attachment: Uri) {
+        val intent = Intent(Intent.ACTION_SEND)
+            intent.type = XLSX_MIME_TYPE
+            intent.putExtra(Intent.EXTRA_EMAIL, addresses)
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_STREAM, attachment)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
         if (intent.resolveActivity(requireActivity().packageManager) != null) {
             startActivity(intent)
         }
