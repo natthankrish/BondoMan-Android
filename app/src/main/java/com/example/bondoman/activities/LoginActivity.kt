4404b54330446e01acbae8a25ccbe7a2b2a5724b
Natthan Krish <natthankrish@Neos-MacBook-Pro.local>
2024-04-01 21:50:07 +0700

    feat: anim login

--- app/src/main/java/com/example/bondoman/activities/LoginActivity.kt
+++ app/src/main/java/com/example/bondoman/activities/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.bondoman.activities
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.DisplayMetrics
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.lifecycleScope
 import com.example.bondoman.R
 import com.example.bondoman.apiServices.IAuthService
@@ -13,6 +21,7 @@ import com.example.bondoman.lib.SecurePreferences
 import com.example.bondoman.repositories.LoginRepository
 import com.example.bondoman.retrofits.Retro
 import kotlinx.coroutines.launch
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var email : EditText
@@ -22,6 +31,23 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Handler().postDelayed({
+            // Transition to the login screen after SPLASH_DURATION
+            val cardLogin = findViewById<CardView>(R.id.login_card)
+            val logo = findViewById<ImageView>(R.id.imageView)
+
+            val slideUpAnimation = AnimationUtils.loadAnimation(this@LoginActivity, R.anim.slide_up)
+            val finalY = cardLogin.y + (cardLogin.height - logo.height) / 2
+
+            val animatorY = ObjectAnimator.ofFloat(logo, "translationY", 0f + logo.height, finalY)
+            animatorY.duration = 600
+
+            animatorY.start()
+            cardLogin.startAnimation(slideUpAnimation)
+            cardLogin.visibility = CardView.VISIBLE
+        }, 1000)
+
 
         val authService = Retro().getRetroClientInstance().create(IAuthService::class.java)
         val securePreferences = SecurePreferences(this)
