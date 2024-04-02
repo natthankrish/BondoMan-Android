e8bf0bed6f66b1b2104e026ae3d55c2d9e3d6985
Natthan Krish <natthankrish@Neos-MacBook-Pro.local>
2024-04-02 10:25:59 +0700

    fix: main horizontal

--- app/src/main/java/com/example/bondoman/activities/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/activities/MainActivity.kt
@@ -4,19 +4,30 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.onNavDestinationSelected
+import androidx.viewpager2.adapter.FragmentViewHolder
 import com.example.bondoman.R
 import com.example.bondoman.services.TokenCheckService
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tokenExpiredReceiver: BroadcastReceiver
     private lateinit var tokenServiceIntent : Intent
     private var isReceiverRegistered = false
+    private lateinit var bottomNavigationView: BottomNavigationView;
+    private lateinit var navigationView: NavigationView;
+    private lateinit var fragment: NavHostFragment;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,14 +52,58 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        val bottomNavigationView = findViewById<BottomNavigationView
+        bottomNavigationView = findViewById<BottomNavigationView
                 >(R.id.bottom_navigation_view)
         val navController = findNavController(R.id.nav_fragment)
+        navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        fragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        val navFragmentLayoutParams = fragment.view?.layoutParams as ConstraintLayout.LayoutParams
+
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Change layout to show left navigation
+            bottomNavigationView.visibility = View.GONE
+            navigationView.visibility = View.VISIBLE
+            navFragmentLayoutParams.leftToRight = R.id.navigation_view
+            fragment.view?.layoutParams = navFragmentLayoutParams
+        } else {
+            // Set up bottom navigation bar
+            bottomNavigationView.visibility = View.VISIBLE
+            navigationView.visibility = View.GONE
+            navFragmentLayoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
+            fragment.view?.layoutParams = navFragmentLayoutParams
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem : MenuItem ->
+            supportActionBar?.title = menuItem.title
+            menuItem.onNavDestinationSelected(navController)
+            true
+        }
 
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             supportActionBar?.title = menuItem.title
             menuItem.onNavDestinationSelected(navController)
             true
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val orientation = newConfig.orientation
+
+        val navFragmentLayoutParams = fragment.view?.layoutParams as ConstraintLayout.LayoutParams
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Change layout to show left navigation
+            bottomNavigationView.visibility = View.GONE
+            navigationView.visibility = View.VISIBLE
+            navFragmentLayoutParams.leftToRight = R.id.navigation_view
+            fragment.view?.layoutParams = navFragmentLayoutParams
+        } else {
+            // Change layout to show bottom navigation bar
+            bottomNavigationView.visibility = View.VISIBLE
+            navigationView.visibility = View.GONE
+            navFragmentLayoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
+            fragment.view?.layoutParams = navFragmentLayoutParams
         }
     }
 
